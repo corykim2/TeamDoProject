6eb94f7fe8005a2cd2e60adf610a278b5e41c8e6
corykim2 <sos2591@naver.com>
2025-09-22 21:49:52 +0900

    first commit

---
+++ src/main/java/com/TeamAA/TeamDo/TeamDoApplication.java
@@ -0,0 +1,14 @@
+package com.TeamAA.TeamDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//이 클래스 실행하면 서버 돌아감
+@SpringBootApplication
+public class TeamDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TeamAA/TeamDo/TeamDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TeamAA.TeamDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
