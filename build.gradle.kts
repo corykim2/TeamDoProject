0f62d29dda6c2fa201af6483efa145110443de3f
rladjwls02 <safhao7151@naver.com>
2025-10-01 22:52:37 +0900

    chore: Thymeleaf 의존성 추가 및 Spring Security 임시 비활성화
    thymeleaf의존성 추가 및 Spring Security 주석처리

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	// implementation("org.springframework.boot:spring-boot-starter-security")
+	//테스트용으로 잠시 주석처리
+	//나중에 로그인 구현하고 다시 입력
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
