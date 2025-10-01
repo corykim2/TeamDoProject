7bca9bea04fe555b30710acb2e9917712ff96ac0
rladjwls02 <safhao7151@naver.com>
2025-10-01 22:53:56 +0900

    remove: 테스트 파일 삭제
    DB연동 테스트파일 TestDB.java, TestUser.java, TestUserRepository.java 삭제

--- src/main/java/com/TeamAA/TeamDo/repository/TestUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.TeamAA.TeamDo.repository;
-
-import com.TeamAA.TeamDo.entity.TestUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestUserRepository extends JpaRepository<TestUser, Long> {
-    TestUser findByUsername(String username);
-}
