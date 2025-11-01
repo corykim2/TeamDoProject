31e0bf0d73252daa7ab897e3a815b43df8ee2bc6
rladjwls02 <safhao7151@naver.com>
2025-11-01 23:35:42 +0900

    feat: LoginController 생성

--- src/main/java/com/TeamAA/TeamDo/controller/LoginController.java
+++ src/main/java/com/TeamAA/TeamDo/controller/LoginController.java
@@ -1,4 +1,15 @@
 package com.TeamAA.TeamDo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
 public class LoginController {
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
 }
