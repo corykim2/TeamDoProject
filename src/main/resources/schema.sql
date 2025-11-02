22664dbb269d632dbd3cc00b9d770122faf3a181
dlcksdud <dlcksdud124@gmail.com>
2025-11-02 15:14:38 +0900

    feat:엔티티 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,61 +1,63 @@
 -- 1. User 테이블
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    email_address VARCHAR(64) NOT NULL,
+    emailAddress VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL,
-    user_name VARCHAR(64) NOT NULL,
+    userName VARCHAR(64) NOT NULL,
     organization VARCHAR(64),
-    is_delete BOOLEAN NOT NULL
+    isDelete BOOLEAN NOT NULL
 );
 
 -- 2. Team 테이블
 CREATE TABLE team (
-    team_code INT PRIMARY KEY AUTO_INCREMENT,
-    leader_id INT,
-    FOREIGN KEY (leader_id) REFERENCES users(id)
+    teamCode INT PRIMARY KEY AUTO_INCREMENT,
+    leaderId INT,
+    FOREIGN KEY (leaderId) REFERENCES users(id)
 );
 
 -- 3. Project 테이블
 CREATE TABLE project (
-    p_no INT PRIMARY KEY AUTO_INCREMENT,
-    p_name VARCHAR(64) NOT NULL,
-    progress_rate DOUBLE NOT NULL,
-    team_code INT,
-    FOREIGN KEY (team_code) REFERENCES team(team_code)
+    pNo INT PRIMARY KEY AUTO_INCREMENT,
+    pName VARCHAR(64) NOT NULL,
+    progressRate DOUBLE NOT NULL,
+    teamCode INT,
+    FOREIGN KEY (teamCode) REFERENCES team(teamCode)
 );
 
 -- 4. Todo 테이블
 CREATE TABLE todo (
-    p_no INT PRIMARY KEY,
-    priority int NOT NULL,
+    todoId INT PRIMARY KEY AUTO_INCREMENT, -- 고유 식별자 (기본 키)
+    pNo INT NOT NULL,                      -- Project 외래 키
+    priority INT NOT NULL,
     state VARCHAR(64) NOT NULL,
     deadline DATE NOT NULL,
-    manager VARCHAR(64) NOT NULL,
-    name VARCHAR(64) NOT NULL,
-    FOREIGN KEY (p_no) REFERENCES project(p_no)
+    managerId INT NOT NULL,                -- User 외래 키 (관리자 ID)
+    name VARCHAR(64) NOT NULL,              -- 할 일 이름 (NOT NULL 권고)
+    FOREIGN KEY (pNo) REFERENCES project(pNo),
+    FOREIGN KEY (managerId) REFERENCES users(id)
 );
 
 -- 5. TeamMembership 테이블
 CREATE TABLE team_membership (
-    team_code INT,
-    user_id INT,
-    PRIMARY KEY (team_code, user_id),
-    FOREIGN KEY (team_code) REFERENCES team(team_code),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    teamCode INT,
+    userId INT,
+    PRIMARY KEY (teamCode, userId),
+    FOREIGN KEY (teamCode) REFERENCES team(teamCode),
+    FOREIGN KEY (userId) REFERENCES users(id)
 );
 
 CREATE TABLE invite_code(
-    invite_id INT PRIMARY KEY,
+    inviteId INT PRIMARY KEY,
     code VARCHAR(64) NOT NULL,
     state VARCHAR(64) NOT NULL,
-    p_no INT,
-    FOREIGN KEY (p_no) REFERENCES project(p_no)
+    pNo INT,
+    FOREIGN KEY (pNo) REFERENCES project(pNo)
 );
 
 CREATE TABLE session(
-    s_id VARCHAR(64) PRIMARY KEY,
-    u_id INT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    expires_at TIMESTAMP NOT NULL,
-    FOREIGN KEY (u_id) REFERENCES users(id)
+    sId VARCHAR(64) PRIMARY KEY,
+    uId INT NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    expiresAt TIMESTAMP NOT NULL,
+    FOREIGN KEY (uId) REFERENCES users(id)
 );
