0602f0efab225f4ee28dd8508f88ee8ec5bf4bab
dlcksdud <dlcksdud124@gmail.com>
2025-11-13 13:55:41 +0900

    feat: 정렬 api추가

--- src/main/java/com/TeamAA/TeamDo/service/TodoService.java
+++ src/main/java/com/TeamAA/TeamDo/service/TodoService.java
@@ -1,8 +1,12 @@
 package com.TeamAA.TeamDo.service;
 
+import com.TeamAA.TeamDo.dto.TodoUpdateRequest;
 import com.TeamAA.TeamDo.entity.TodoEntity;
 import com.TeamAA.TeamDo.repository.TodoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,7 +19,6 @@ public class TodoService {
 
     // 1. 할 일 생성
     public TodoEntity createTodo(TodoEntity todo) {
-        // ⭐️ 여기서 데이터 유효성 검사, 권한 확인 등 비즈니스 로직 수행
         return todoRepository.save(todo);
     }
 
@@ -37,11 +40,44 @@ public class TodoService {
         todo.setState(newState);
         return todoRepository.save(todo);
     }
-    //5. 할일 삭제
+    //5. 할일 삭제(본인만 삭제가능 하도록 위해서는 로그인 기능 필요)
     public TodoEntity deleteTodo(Integer todoId){
         TodoEntity todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new RuntimeException("해당 Todo가 존재하지 않습니다."));
         todoRepository.delete(todo);
         return todo;
+    }
+    //6. 할일 수정
+    public TodoEntity updateTodoFields(Integer todoId, TodoUpdateRequest request) {
+        TodoEntity todo = todoRepository.findById(todoId)
+                .orElseThrow(() -> new EntityNotFoundException("할 일을 찾을 수 없습니다."));
+/*
+//로그인기능 필요
+        if (!todo.getMangerId().equals(currentUserId)) {
+            // 권한이 없는 경우, HTTP 403 Forbidden 에러를 발생시킬 예외를 던집니다.
+            throw new AccessDeniedException("수정 권한이 없습니다. 본인의 할 일만 수정할 수 있습니다.");
+        }
+*/
+        if (request.getName() != null) {
+            todo.setName(request.getName());
+        }
+        if (request.getDeadline() != null) {
+            todo.setDeadline(request.getDeadline());
+        }
+        if (request.getState() != null) {
+            todo.setState(request.getState());
+        }
+        if (request.getPriority() != null) {
+            todo.setPriority(request.getPriority());
+        }
+
+        return todoRepository.save(todo);
+    }
+    //정렬기능
+    public List<TodoEntity> findAllTodosSorted(String sortBy, String direction) {
+        Sort.Direction sortDirection = direction.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Sort sort = Sort.by(sortDirection, sortBy);
+
+        return todoRepository.findAll(sort);
     }
 }
