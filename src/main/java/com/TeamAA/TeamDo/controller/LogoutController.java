dd1f887d4f1279497da69641ca31005ad8271c2d
rladjwls02 <safhao7151@naver.com>
2025-11-16 17:05:46 +0900

    feat: 로그아웃, 회원탈퇴 예외처리기능 추가

--- src/main/java/com/TeamAA/TeamDo/controller/LogoutController.java
+++ src/main/java/com/TeamAA/TeamDo/controller/LogoutController.java
@@ -15,8 +15,14 @@ public class LogoutController {
 
     @PostMapping("/logout")
     public ResponseEntity<String> logout(HttpSession session) {
-        String userId = sessionService.getUserId(session); //세션검증
-        session.invalidate();
-        return ResponseEntity.ok("로그아웃 완료");
+        //정상 처리 로직
+        try {
+            String userId = sessionService.getUserId(session); //세션검증
+            session.invalidate();
+            return ResponseEntity.ok("로그아웃 완료");
+        } catch (Exception e){
+            // 내부 서버 예외
+            throw new RuntimeException("로그아웃 처리 중 문제가 발생하였습니다. 다시 시도해주세요.", e);
+        }
     }
 }
--- src/main/java/com/TeamAA/TeamDo/service/DeleteUserService.java
+++ src/main/java/com/TeamAA/TeamDo/service/DeleteUserService.java
@@ -16,15 +16,28 @@ public class DeleteUserService {
 
     @Transactional
     public UserEntity deleteUser(String Id, DeleteUserRequest request) {
-        UserEntity user = userRepository.findById(Id)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
-        // 해싱 비밀번호 검증
-        if (!BCrypt.checkpw(request.getPassword(), user.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
 
-        user.setWithdrawn(true);
-        userRepository.save(user);
-        return user;
+        try {// 비밀번호 입력 누락 예외 처리
+            if (request.getPassword() == null || request.getPassword().isBlank()) {
+                throw new IllegalArgumentException("비밀번호를 입력해주세요");
+            }
+            // 사용자 조회
+            UserEntity user = userRepository.findById(Id)
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+            // 비밀번호 불일치 예외 처리
+            if (!BCrypt.checkpw(request.getPassword(), user.getPassword())) {
+                throw new IllegalArgumentException("비밀번호가 일치하지 않습니다, 올바른 비밀번호를 입력해주세요.");
+            }
+
+            // 탈퇴 처리
+            user.setWithdrawn(true);
+            userRepository.save(user);
+
+            return user;
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("회원탈퇴 처리 중 문제가 발생하였습니다. 다시 시도해주세요.", e);
+        }
     }
 }
